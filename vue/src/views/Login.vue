2f95707a731a611013c4c41ee663a46115595d0c
杨洹 <827767408@qq.com>
2022-12-01 16:24:37 +0800

    init

---
+++ vue/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+<div style="width: 100%;height: 100vh ;background-color: burlywood;overflow: hidden">
+  <div style="width: 1000px;margin: 150px auto">
+    <div style="color: #42b983;font-size: 50px;text-align: center">欢迎消费</div>
+   <el-form-item label="youxiang">
+     <el-input type="text">请输入用户名</el-input>
+
+   </el-form-item>
+
+
+  </div>
+</div>
+
+</template>
+<script>
+
+export default {
+  name:"login",
+  components:{
+
+  }
+}
+</script>
+<style>
+
+</style>
